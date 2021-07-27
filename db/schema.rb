854d41edb1b1934ae2a248009b88a3363ea0f26d
tk <t.k.cloud9112@gmail.com>
2021-07-27 08:14:28 +0000

    second

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_071025) do
+ActiveRecord::Schema.define(version: 2021_07_26_132323) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "title"
   end
 
 end
